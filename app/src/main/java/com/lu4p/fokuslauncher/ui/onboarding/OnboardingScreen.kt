7bfd025407bf56a5c2e5fae76a0050a1dc588991
Paul Scheduikat <lu4p@pm.me>
2026-02-20 04:23:04 +0100

    fix onboarding background

--- app/src/main/java/com/lu4p/fokuslauncher/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/lu4p/fokuslauncher/ui/onboarding/OnboardingScreen.kt
@@ -1,6 +1,7 @@
 package com.lu4p.fokuslauncher.ui.onboarding
 
 import android.Manifest
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -39,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -77,6 +79,7 @@ fun OnboardingScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.Black)
             .padding(32.dp)
     ) {
         Spacer(modifier = Modifier.weight(1f))
