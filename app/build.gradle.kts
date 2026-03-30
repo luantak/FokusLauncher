02dec632c9f9c9601500b3f74290298c8c257e49
Paul Scheduikat <lu4p@pm.me>
2026-03-30 21:09:09 +0200

    prepare 1.1.11 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "io.github.luantak.fokuslauncher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.1.10"
+        versionCode = 25
+        versionName = "1.1.11"
 
         testInstrumentationRunner = "io.github.luantak.fokuslauncher.HiltTestRunner"
     }
